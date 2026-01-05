05e079451fec6e15228c13d518613ab3b69cacaa
Jay Chinchkar <jrchinchkar16@gmail.com>
2026-01-05 17:07:28 +0530

    fix: casing for Login import to resolve Vercel build error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 
